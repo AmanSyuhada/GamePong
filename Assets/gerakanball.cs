50512ce0a8ba284b902d956b71108d9c07e77c3a
AmanSyuhada <syuhadaamand13@gmail.com>
2020-05-10 02:57:55 -0700

    commit semuanya

---
+++ Assets/gerakanball.cs
@@ -0,0 +1,51 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class gerakanball : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int speed;
+
+    public Rigidbody2D ball;
+    public Animator FireBall;
+    public GameObject MasterScript;
+
+    public AudioSource hitEFffect;
+
+    void Start()
+    {
+        ball.velocity = new Vector2(1,-1) * speed;
+        FireBall.SetBool("ismoving", true);
+    }
+
+    // Update is called once per frame 
+    void FixedUpdate()
+    {
+        if(ball.velocity.x > 0){
+        	ball.GetComponent<Transform>().localScale = new Vector3(1, 1, 1);
+        	}else{
+        		ball.GetComponent<Transform>().localScale = new Vector3(-1, 1, 1);
+        	}
+    }
+
+    void OnCollisionEnter2D(Collision2D other){
+    	if(other.collider.name=="Kanan" || other.collider.name=="Kiri"){
+    		MasterScript.GetComponent<ScoringScript>().UpdateScore(other.collider.name);
+    		StartCoroutine(interval());
+    	}
+        if (other.collider.tag=="Player"){ 
+            hitEFffect.Play();
+        }
+    } 
+
+    IEnumerator interval(){
+    	ball.velocity = new Vector2(0,0);
+    	FireBall.SetBool("ismoving", false);
+    	ball.GetComponent<Transform>().position = new Vector2(0,0);
+    	yield return new WaitForSeconds(1);
+    	ball.velocity = new Vector2(-1,-1) * speed;
+    	FireBall.SetBool("ismoving", true);
+    }
+}
